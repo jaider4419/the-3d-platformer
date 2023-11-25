495785bc6ff2d886bf9eba88960d477c9b8343bc
jaider4419 <jaiderboateng8@gmail.com>
2023-11-25 23:50:39 +0000

    radiohead time

---
+++ Assets/CameraController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class CameraController : MonoBehaviour
+{
+
+    public PlayerController player;
+    private float sensitivity = 500f;
+    private float clampAngle = 85f;
+
+    private float verticalRotation;
+    private float horizontalRotation;
+
+    private void Start()
+    {
+        this.verticalRotation = this.transform.localEulerAngles.x;
+        this.horizontalRotation = this.transform.localEulerAngles.y;
+    }
+
+    private void Update()
+    {
+        Look();
+        Debug.DrawRay(this.transform.position, this.transform.forward * 2, Color.red);
+    }
+
+    private void Look()
+    {
+        float mouseVertical = Input.GetAxis("mouse Y");
+        float mouseHorizontal = Input.GetAxis("mouse X");
+
+        this.verticalRotation += mouseVertical * this.sensitivity * Time.deltaTime;
+        this.horizontalRotation += mouseHorizontal * this.sensitivity * Time.deltaTime;
+
+        this.verticalRotation = Mathf.Clamp(this.verticalRotation, -this.clampAngle, this.clampAngle);
+
+        this.transform.localRotation = Quaternion.Euler(this.verticalRotation, 0f, 0f);
+        this.player.transform.rotation = Quaternion.Euler(0f, this.horizontalRotation, 0f);
+    }
+}
