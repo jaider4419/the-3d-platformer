fa2ca875ed6415ba55c05c9324291070f796a199
jaider4419 <jaiderboateng8@gmail.com>
2023-11-28 22:15:14 +0000

    almost completely finished the game

---
+++ Assets/Scripts/MainStory.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainStory : MonoBehaviour
+{
+
+    void OnEnable()
+    {
+        SceneManager.LoadScene("Start Scene", LoadSceneMode.Single);
+    }
+}
