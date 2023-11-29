b9e51dc0ba2e20edf24202deb55236044ca8b6bf
jaider4419 <jaiderboateng8@gmail.com>
2023-11-29 23:31:14 +0000

    i hate modelliong

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         this.rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void FixedUpdate()
